af6bccd1f3fa31c7b70c52d06cda90fe4d306c5d
Tim <trclubcar@gmail.com>
2014-07-04 04:38:33 -0400

    Cleaned up the pins scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Pinteresting::Application.routes.draw do
+  resources :pins
+
   devise_for :installs
   devise_for :users
-root "pages#home" #root_path is defined here
-get "about" => "pages#about" #about_path is defined here
+  root "pages#home" #root_path is defined here
+  get "about" => "pages#about" #about_path is defined here
  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
